e533536a9a20407faea4a4912a7043778cfb5cfc
Muhammad Sulman <sulman.aaay@gmail.com>
2021-04-14 20:04:13 +0100

    MyChanges

---
+++ src/GildedRose.Console/Shop.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRose.Application
+{
+    public static class Shop
+    {
+        public static IList<Item> Items;
+
+        public static void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                changeQuality(Items[i])
+                decreaseSellIn(Items[i])
+
+
+                if (Items[i].SellIn < 0)
+                {
+                    decreaseSellInQuality(item);
+                }
+                changeSellInQuality(item){
+                    if (item.Name != "Aged Brie")
+                    {
+                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                decreaseQuality(item)
+                            }
+                        }
+                        else
+                        {
+                            item.Quality = item.Quality - item.Quality;
+                        }
+                    }
+                    else
+                    {
+                        increaseQuality(item);
+                    }
+                }
+            }
+        }
+
+        changeQuality(item)
+        {
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" &&
+                item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                if (Items[i].Quality > 0)
+                {
+                    decreaseQuality(item)
+                }
+            }
+            else
+            {
+                if (Items[i].Quality < 50)
+                {
+                    increaseQuality(item)
+                }
+            }
+        }
+
+        decreaseSellIn(item)
+        {
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                item.SellIn = item.SellIn - 1;
+            }
+        }
+
+        decreaseQuality(item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+            }
+        }
+        increaseQuality(item)
+        {
+
+        }
+
+    }
+}
